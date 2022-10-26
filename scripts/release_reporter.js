f69553a421fa3f904dfed5f1759beb2f800a83d9
Roman Kolerov <coder8080@mail.ru>
2022-10-26 18:31:43 +0300

    Добавление комментария о релизе

---
+++ scripts/release_reporter.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+const process = require('process');
+
+const main = async () => {
+  const authorization_key = process.env.secrets.TRACKER_AUTHORIZATION_KEY;
+  const text = JSON.stringify(process.github);
+  console.log(process.env);
+  console.log(authorization_key);
+  const result = await axios.post(
+    'https://api.tracker.yandex.net/v2/issues/HOMEWORKSHRI-169/comments',
+    {
+      text,
+    },
+    {
+      headers: {
+        Authorization: 'OAuth ' + authorization_key,
+        'X-Org-ID': '7526988',
+      },
+    }
+  );
+};
+
+main();
